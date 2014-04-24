a039ccb5be9ae141de047a33c63541064fa1632a
Hack Reactor Students <adrice727@gmail.com>
2014-04-23 19:45:55 -0700

    Fixed countNQueensSolutions test bug

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -42,8 +42,8 @@ describe('solvers', function() {
 
   describe('countNQueensSolutions()', function() {
 
-    it('finds the number of valid solutions for n of 0-8', function() {
-      _.range(0, 9).map(function(n) {
+    it('finds the number of valid solutions for n of 1-8', function() {
+      _.range(1, 9).map(function(n) {
         var solutionCount = countNQueensSolutions(n);
         var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92][n];
 
